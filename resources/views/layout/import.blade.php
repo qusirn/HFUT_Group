5847f1512e3b3ae5fdfe47cfa4de17074546b183
vagrant <vagrant@homestead>
2018-03-25 13:41:11 +0000

    修改直播间页面代码风格 Ref #15

--- resources/views/layout/import.blade.php
+++ resources/views/layout/import.blade.php
@@ -2,13 +2,11 @@
 <script src="/semantic/dist/semantic.min.js"></script>
 <link rel="stylesheet" type="text/css" href="/semantic/dist/components/reset.css">
 <link rel="stylesheet" type="text/css" href="/semantic/dist/components/site.css">
-
 <link rel="stylesheet" type="text/css" href="/semantic/dist/components/container.css">
 <link rel="stylesheet" type="text/css" href="/semantic/dist/components/grid.css">
 <link rel="stylesheet" type="text/css" href="/semantic/dist/components/header.css">
 <link rel="stylesheet" type="text/css" href="/semantic/dist/components/image.css">
 <link rel="stylesheet" type="text/css" href="/semantic/dist/components/menu.css">
-
 <link rel="stylesheet" type="text/css" href="/semantic/dist/components/divider.css">
 <link rel="stylesheet" type="text/css" href="/semantic/dist/components/list.css">
 <link rel="stylesheet" type="text/css" href="/semantic/dist/components/segment.css">
