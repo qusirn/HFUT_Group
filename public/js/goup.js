ec3f11d54caaadff947b8cb26deb23a303089736
tangruo <1163488165@qq.com>
2018-04-11 21:52:40 +0800

    修改个人信息页面编写以及重写个人中心页面 Ref #41

--- public/js/goup.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function($) {
-                $('.counter').counterUp({
-                    delay: 100,
-                    time: 1200
-                });
-            });
